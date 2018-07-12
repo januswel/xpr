fb333be19fb426a2e9a222e474919f32043ea900
Takagi Kensuke <janus.wel.3@gmail.com>
2018-07-12 13:20:05 +0900

    :heavy_plus_sign::sparkles: add cli interface

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // @flow
 
+import cac from 'cac'
 import generate from './src/generate'
 
-console.log(generate(32))
+const DEFAULT_DIGIT = 32
+
+const cli = cac()
+cli.command(
+  'generate',
+  {
+    desc: 'generates random string that has specified length',
+  },
+  input => {
+    const digit = input[0] || DEFAULT_DIGIT
+    console.log(generate(digit))
+  },
+)
+
+cli.parse()
